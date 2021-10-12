c83a905afe9f16f9371ae4182b99867db6f62aef
NirmalNivithaa <92011024+NirmalNivithaa@users.noreply.github.com>
2021-10-12 20:04:15 +0530

    eligibility for vote.cpp
    this program executed eligibility for vote

---
+++ eligibility for vote.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter your age:";
+    cin>>age;
+    if(age>=18)
+    {
+       cout<<"\n you are eligible";
+    }
+    else
+       cout<<"\n you are not eligible";
+return 0;
+}
+       
+       
+       
